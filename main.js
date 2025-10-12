95f88bb68b3e3d3d995dd5f7b7b34b221f016509
turirandy <turirandy@gmail.com>
2025-10-12 13:30:56 +0000

    .

--- main.js
+++ main.js
@@ -1,70 +1,28 @@
-const input = document.getElementById('input')
+const input = document.getElementById('input');
+const colorPicker= document.getElementById('color');
+const recordingToggle = document.getElementById('record')
+const vol_slider = document.getElementById('vol-slider');
 var timepernote = 0;
 var length = 0;
-var reset = false;
-var interval = null;
-var amplitude = 40;
 
-var blob, recorder =null;
-var chunks =[];
-
-function startRecording(){
-  const canvasStrem = canvas.captureStream(20);//frame rate canvas
-  const audioDestination = audioCtx.createMediaStreamDestination();
-  gainNode.connect(audioDestination);
-  const combinedStream=new MediaStream;
-  CanvasCaptureMediaStreamTrack.getVidieoTracks().forEach(track =>combinedStream.addTrack(track));
-  audioDestination.stream.getAudioTracks().forEach(track=> combinedStream.addTrack(track));
-  recorder = new MediaRecorder(combinedStream, { mimeType:'video/webm'});
-  recorder.ondataavailable = e => {
- if (e.data.size > 0) {
-   chunks.push(e.data);
- }
-};
-recorder.onstop = () => {
-   const blob = new Blob(chunks, { type: 'video/webm' });
-   const url = URL.createObjectURL(blob);
-   const a = document.createElement('a');
-   a.href = url;
-   a.download = 'recording.webm';
-   a.click();
-   URL.revokeObjectURL(url);
-};
-recorder.start();
-
-var is_recording = false;
-function toggle(){
-  is_recording =!is_recording;
-  if(is_recording){
-    recording_toggle.innerHTML ="Stop Recording";
-    startRecording();
-  }else{
-    recording_toggle.innerHTML ="Start Recording";
-    recorder.stop();
-  }
-
-}
-}
-const recording_toggle=
-document.getElementById('record')
-
-const color_picker=
-document.getElementById('color')
+///define canvas variables
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext("2d"); 
+  var blob, recorder =null;
+  var chunks =[];
+  var width = ctx.canvas.width;
+  var height = ctx.canvas.height;
+  var is_recording = false;
+  
+  var freq;
+  var y= height / 2;
+  var counter = 0;
+  var x= 0;
+  var interval= null;
+  let reset = false;
 
 
-// create web audio api elements
-const audioCtx = new AudioContext();
-const gainNode = audioCtx.createGain();
-
-
-// create Oscillator node
-const oscillator = audioCtx.createOscillator();
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-oscillator.type ="sine";
-oscillator.start();
-gainNode.gain.value = 0;
-
+const notenames =new Map
 notenames = new Map();
 notenames.set("C", 261.6);
 notenames.set("D", 293.7);
@@ -75,26 +33,131 @@ notenames.set("A", 440);
 notenames.set("B", 493.9);
 
 
+
+  function drawWave() {
+    
+    clearInterval(interval);
+     counter = 0;
+     if(reset){
+     ctx.clearRect(0, 0, width, height);
+     x = 0
+     y = height/2;
+     ctx.beginPath();
+     ctx.moveTo(x, y);
+    }
+    interval = setInterval(line, 20);
+    reset = false;
+  }
+
+
+ function line(){
+  
+  y = height/2 + (vol_slider.value/100)*40 *Math.sin(x * 2 * Math.PI*freq* (0.5* length));
+  ctx.lineTo(x,y);
+  ctx.stroke();
+  x = x + 1;
+  ctx.srokeStyle = color_picker.value;
+  
+  //increase counter by 1 to show how long interval has been run
+  counter++;
+
+    if(counter > timepernote/20) {
+    clearInterval(interval);
+  }
+ }
+
+
+var interval = null;
+var amplitude = 40;
+
+
+
+function startRecording(){
+  const canvasStrem = canvas.captureStream(20);//frame rate canvas
+  const audioDestination = audioCtx.createMediaStreamDestination();
+  gainNode.connect(audioDestination);
+  const combinedStream=new MediaStream;
+  CanvasCaptureMediaStreamTrack.getVidieoTracks().forEach(track =>combinedStream.addTrack(track));
+  audioDestination.stream.getAudioTracks().forEach(track=> combinedStream.addTrack(track));
+  
+  recorder = new MediaRecorder(combinedStream, { mimeType:'video/webm'});
+  recorder.ondataavailable = e => {
+ if (e.data.size > 0) {
+   chunks.push(e.data);
+ }
+};
+recorder.onstop = () => {
+   blob = new Blob(chunks, { type: 'video/webm' });
+   const url = URL.createObjectURL(blob);
+   const a = document.createElement('a');
+   a.href = url;
+   a.download ="Musicalworm_recording.webm"
+   a.click();
+   URL.revokeObjectURL(url);
+};
+recorder.start();
+console.log("Recording started")
+}
+
+function Toggle(){
+  is_recording =!is_recording;
+  if(is_recording){
+    recording_toggle.innerHTML ="Stop Recording";
+    startRecording();
+  }else{
+    recording_toggle.innerHTML ="Start Recording";
+    recorder.stop();
+  }
+
+}
+const recording_toggle= document.getElementById("click , toggle")
+
+const color_picker=
+document.getElementById('color')
+
+
+// create web audio api elements
+const audioCtx = new AudioContext();
+
+
+// create Oscillator node
+oscillator.start();
+gainNode.gain.value = 0;
+
+
 function frequency(pitch){
-  freq = pitch/10000;
+
+  const oscillator = audioCtx.createOscillator();
+  const gainNode = audioCtx.createGain();
+  
+  oscillator.type ="sine";
+  oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
 
   gainNode.gain.setValueAtTime(vol_slider, audioCtx.currentTime);
   setting = setInterval (() => {gainNode.gain.value = vol_slider.slider.value}, 1);
-  oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-  setTimeout(() => {clearInterval(setting); gainNode.gain.value= 0;}, ((timepernote)-10)),
-  setTimeout(() => gainNode.gain.setValueAtTime(0, audioCtx.currentTime + (timepernote/1000 - 0.1)));
+  setTimeout(() => {clearInterval(setting);
+    gainNode.gain.value = 0},((timepernote)- 10));
+    setTimeout(() => gainNode.gain.setValueAtTime(0, audioCtx.currentTime + (timepernote/1000 - 0.1)));
 
-
+  oscillator.stop(audioCtx.currentTime + 1);
+  oscillator.start();
+  
+  freq = pitch/10000;
 }
 
 function handle(){
   reset = true
   audioCtx.resume();
+
   gainNode.gain.value = 0;
   var usernotes = String(input.value);
+  var noteslist = [];
+
   length = usernotes.length;
   timepernote = (6000 / length);
-  var noteslist = [];
   
   for (i =0; i < usernotes.length; i++){
      noteslist.push(notenames.get(usernotes.charAt(i)));
@@ -112,43 +175,6 @@ function handle(){
 
       }, timepernote)
     
-  }
+  };
  
-  //define canvas variables
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d"); 
-  var counter = 0;
-
-  var width = ctx.canvas.width;
-  var height = ctx.canvas.height;
- 
-  function drawWave() {
-    clearInterval(interval);
-
-     counter = 0;
-     interval = setInterval(line, 20);
-     if(reset){
-     ctx.clearRect(0, 0, width, height);
-     x = 0
-     y = height/2;
-     ctx.moveTo(x, y);
-     ctx.beginPath();
-    }
-    reset = false;
-  }
-
-
- function line(){
   
-  y = height/2 + (vol_slider.value/100)*40 *Math.sin(x * 2 * Math.PI*freq* (0.5* length));
-  ctx.lineTo(x,y);
-  ctx.stroke();
-  x = x + 1;
-  
-  //increase counter by 1 to show how long interval has been run
-  counter++;
-
-    if(counter > timepernote/20) {
-    clearInterval(interval);
-  }
- }
